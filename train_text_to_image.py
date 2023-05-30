57358d8669bbeab747cf25eb04ac0d12ff0f41f5
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2023-05-30 13:18:27 -0700

    Fix

--- train_text_to_image.py
+++ train_text_to_image.py
@@ -1053,7 +1053,7 @@ def main():
             if args.freeze_unet:
                 logs["lr"] = lr_scheduler_discriminator.get_last_lr()[0]
                 if args.autolr:
-                    logs["autolr"] = lr_scheduler_discriminator.get_last_lr()[0] * optimizer_discriminator.lr_mult
+                    logs["autolr"] = lr_scheduler_discriminator.get_last_lr()[0] * optimizer_discriminator.optimizer.lr_mult
             else:
                 logs["lr"] = lr_scheduler.get_last_lr()[0]
                 logs["mse_loss"] = mse_loss.item()
