e3674ba5bbed1bed562b3f27533b2dfc971cd86d
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2024-03-06 20:17:40 -0800

    Update train_text_to_image.py

--- train_text_to_image.py
+++ train_text_to_image.py
@@ -1173,7 +1173,7 @@ def main():
                             
                     if args.weight_p != 0:
                         snr = compute_snr(timestep.to(dtype=torch.long))
-                        grad = grad * snr ** args.weight_p
+                        grad = grad * snr[:,None,None,None] ** args.weight_p
 
                     # Do sample forward pass again, this time with gradient information
                     generator_output = unet(latents, timestep, encoder_hidden_states).sample
